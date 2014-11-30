b40b3976f7d24d2dc15103bb0fec666a38edd433
Andrew <maxparko@Max-Parkos-MacBook-Pro.local>
2014-11-30 14:30:05 -0800

    resolving file conflict

--- content/plugins/bbp-private-groups/bbp-private-groups.php
+++ content/plugins/bbp-private-groups/bbp-private-groups.php
@@ -4,7 +4,7 @@
 Plugin Name: bbP private groups
 Plugin URI: http://www.rewweb.co.uk/bbp-private-groups/
 Description: This plugin adds private groups to the forums, allocating users to groups, and combinations of forums to those groups, creating multiple closed forums.
-Version: 2.3
+Version: 2.4
 Author: Robin Wilson
 Author URI: http://www.rewweb.co.uk
 License: GPL2
