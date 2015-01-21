1871322b8efc990e834e54983eb0c63630330556
fractal-overflow <fractal-overflow@alphadirectorate.com>
2015-01-21 19:51:18 +0200

    modrewrite

--- vagrant/lemp.sh
+++ vagrant/lemp.sh
@@ -11,10 +11,9 @@ then
 
 	sudo apt-get install -y apache2
 
-	sudo a2enmode rewrite
-
 	sudo service apache2 restart
 
+	sudo a2enmode rewrite
 
 #	echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/nginx-stable.list
 #	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
