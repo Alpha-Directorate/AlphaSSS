bf01137a4c18a113848e531c81a6f504d1bf32c9
fractal-overflow <fractal-overflow@alphadirectorate.com>
2015-01-30 00:53:27 +0200

    Updated apache config

--- travis/lamp.sh
+++ travis/lamp.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-if [ ! -f /etc/apache2/sites-available/alphasss.dev.conf ];
-then
-	sudo apt-get install -y mysql-client php5-mysql libapache2-mod-auth-mysql
+sudo apt-get install -y mysql-client php5-mysql libapache2-mod-auth-mysql
 
-	sudo apt-get install -y php5 libapache2-mod-php5 php5-mcrypt
+sudo apt-get install -y php5 libapache2-mod-php5 php5-mcrypt
 
-	sudo apt-get install -y apache2
+sudo apt-get install -y apache2
 
-	sudo service apache2 restart
+sudo service apache2 restart
 
-	sudo a2enmod rewrite
-fi
+sudo a2enmod rewrite
--- travis/wordpress.sh
+++ travis/wordpress.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 
-if [ ! -f /etc/apache2/sites-available/alphasss.dev.conf ];
-then
+# Create database
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS alphasssdev;"
 
-	# Create database
-	mysql -uroot -e "CREATE DATABASE IF NOT EXISTS alphasssdev;"
+# insert data into local database
+mysql -uroot alphasssdev < ./vagrant/dump/dump.sql
 
-	# insert data into local database
-	mysql -uroot alphasssdev < ./vagrant/dump/dump.sql
+# set correct host name
+mysql -uroot -e "use alphasssdev; update wp_options set option_value = 'http://alphasss.dev/wp' where option_id = 3;"
+mysql -uroot -e "use alphasssdev; update wp_options set option_value = 'http://alphasss.dev' where option_id = 4;"
 
-	# set correct host name
-	mysql -uroot -e "use alphasssdev; update wp_options set option_value = 'http://alphasss.dev/wp' where option_id = 3;"
-	mysql -uroot -e "use alphasssdev; update wp_options set option_value = 'http://alphasss.dev' where option_id = 4;"
+sudo rm /etc/apache2/sites-available/000-default
+sudo mv ./travis/etc/apache2/sites-available/alphasss.dev.conf /etc/apache2/sites-available/000-default
 
-	sudo rm /etc/apache2/sites-available/000-default
-	sudo mv ./travis/etc/apache2/sites-available/alphasss.dev.conf /etc/apache2/sites-available/000-default
+# Install composer
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
 
-	# Install composer
-	curl -sS https://getcomposer.org/installer | php
-	sudo mv composer.phar /usr/local/bin/composer
+composer install --dev
 
-	composer install --dev
+sudo rm /etc/hosts 
 
-	sudo rm /etc/hosts 
+sudo cp ./travis/etc/hosts /etc/hosts 
 
-	sudo cp ./travis/etc/hosts /etc/hosts 
-
-	sudo service apache2 restart
-fi
+sudo service apache2 restart
