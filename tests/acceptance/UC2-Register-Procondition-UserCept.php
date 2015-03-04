d128802385fbe9deec06932c2058756e7d3e4372
fractal-overflow <fractal-overflow@alphadirectorate.com>
2015-03-04 17:49:01 +0200

    unit tests updates

--- tests/acceptance/UC2-Register-Procondition-UserCept.php
+++ tests/acceptance/UC2-Register-Procondition-UserCept.php
@@ -9,11 +9,11 @@ $I->seeCurrentUrlEquals('/wp/wp-login.php');
 $I->fillField('log','Founder_Counselor');
 $I->fillField('pwd','#caRousal.72');
 $I->click('Log In');
-$I->seeCurrentUrlEquals('/');
+$I->seeCurrentUrlEquals('/browse/founder_counselor/');
 $I->dontSee('Login','.button');
 $I->dontSee('Register','li');
 $I->click('Log Out');
-$I->seeCurrentUrlEquals('/?loggedout=true');
+$I->seeCurrentUrlEquals('/browse/founder_counselor/?loggedout=true');
 $I->see('Register','li');
 $I->see('Login','.button');
 
