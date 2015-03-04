a33d725e3303a7b727ab9f866d4ebda8715f2aae
fractal-overflow <fractal-overflow@alphadirectorate.com>
2015-03-04 18:02:46 +0200

    unit tests updates

--- travis/wordpress.sh
+++ travis/wordpress.sh
@@ -9,11 +9,27 @@ cat ./vagrant/dump/dump_sql_* > ./vagrant/dump/dump.sql
 mysql -uroot alphasssdev < ./vagrant/dump/dump.sql
 
 # set correct host name
-mysql -uroot -e "use alphasssdev; update wp_options set option_value = 'http://alphasss.dev/wp' where option_id = 3;"
-mysql -uroot -e "use alphasssdev; update wp_options set option_value = 'http://alphasss.dev' where option_id = 4;"
+mysql -uroot -e "use alphasssdev; update wp_options set option_value = 'https://alphasss.dev/wp' where option_id = 3;"
+mysql -uroot -e "use alphasssdev; update wp_options set option_value = 'https://alphasss.dev' where option_id = 4;"
+
+# Enable apache mods
+sudo a2enmod rewrite
+sudo a2enmod ssl
+#--
+
+sudo mkdir /etc/apache2/ssl
+
+sudo cp ./vagrant/etc/apache2/ssl/apache.key /etc/apache2/ssl
+sudo cp ./vagrant/etc/apache2/ssl/apache.crt /etc/apache2/ssl
 
 sudo rm /etc/apache2/sites-available/default
 sudo mv ./travis/etc/apache2/sites-available/alphasss.dev.conf /etc/apache2/sites-available/default
+
+sudo rm /etc/apache2/sites-available/default-ssl.conf
+sudo cp ./vagrant/etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-available
+
+sudo a2ensite alphasss.dev
+sudo a2ensite default-ssl
 
 # Install composer
 curl -sS https://getcomposer.org/installer | php
