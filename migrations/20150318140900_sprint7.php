43dd381013180df8f2d1939ebb199b7dcd7eaf35
fractal-overflow <fractal-overflow@alphadirectorate.com>
2015-03-18 22:03:29 +0200

    ASSS-179

---
+++ migrations/20150318140900_sprint7.php
@@ -0,0 +1,58 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+/**
+ * @todo Move this to the wrapper
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __DIR__ ) . '/wp/' );
+}
+
+define( 'DB_NAME', getenv( 'DB_NAME' ) );
+define( 'DB_USER', getenv( 'DB_USER' ) );
+define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) );
+define( 'DB_HOST', getenv( 'DB_HOST' ) );
+define( 'WP_MEMORY_LIMIT', '96M' );
+
+require_once(ABSPATH . "/wp-load.php");
+
+// Load all the admin APIs, for convenience
+require ABSPATH . 'wp-admin/includes/admin.php';
+//--
+
+class Sprint7 extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     *
+    public function change()
+    {
+    }
+    */
+    
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+		if ( $plugin = (new \WP_CLI\Fetchers\Plugin)->get( 'alphasss-top-bar' ) ) {
+			activate_plugin( $plugin->file, '' );
+		}
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+    	if ( $plugin = (new \WP_CLI\Fetchers\Plugin)->get( 'alphasss-top-bar' ) ) {
+			deactivate_plugins( $plugin->file );
+		}
+    }
+}
