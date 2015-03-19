298daf3a8f435739aaa3552def462a204f9b94f5
Serhii Prokhorenko <grey.psv@gmail.com>
2015-03-19 16:46:31 +0200

    ASSS-321

--- content/plugins/alphasss-members/assets/js/member.js
+++ content/plugins/alphasss-members/assets/js/member.js
@@ -22,6 +22,22 @@ jQuery(document).ready(function($) {
 		}, 600);
 	}
 
+	function reposition() {
+        var modal = $(this),
+            dialog = modal.find('.modal-dialog');
+        modal.css('display', 'block');
+        
+        // Dividing by two centers the modal exactly, but dividing by three 
+        // or four works better for larger screens.
+        dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
+	    }
+	    // Reposition when a modal is shown
+	    $('.modal').on('show.bs.modal', reposition);
+	    // Reposition when the window is resized
+	    $(window).on('resize', function() {
+	        $('.modal:visible').each(reposition);
+	    });
+
 	$('#wp-admin-bar-logout a').click(function(){
 		pubnub.unsubscribe({
 			channel: 'onlineUsers'
