c05a31bc36332b5514dd3dfca23e17d048406a07
fractal-overflow <fractal-overflow@alphadirectorate.com>
2015-06-09 14:05:35 +0300

    ASSS-414 ASSS-415

--- content/themes/buddyboss-child/purchase-credits.php
+++ content/themes/buddyboss-child/purchase-credits.php
@@ -6,8 +6,38 @@ Template Name: Purchase Credits
 
 <?php get_header(); ?>
 
-	<div id="content" role="main">
-		test
+<div class="page-three-columns">
+
+	<div id="secondary" class="widget-area left-widget-area" role="complementary">
+		<aside class="widget widget-error">
+			<img src="https://avatars3.githubusercontent.com/u/432541?v=3&s=460">
+		</aside>
 	</div>
 
+	<div id="primary" class="site-content">
+	
+		<div id="content" role="main">
+
+			<h1 class="entry-title"><?php _e('Select Credits'); ?></h1>
+
+			<p><?php _e('How many Credits would you like to buy?'); ?></p>
+
+			<form >
+				<select>
+					
+				</select>
+
+				<button>Proceed to Payment</button>
+			</form>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+	<div id="secondary" class="widget-area" role="complementary">
+		What are Bitcoins?
+
+		What are Credits?
+	</div>
+
+</div><!-- .page-left-column -->
 <?php get_footer(); ?>
