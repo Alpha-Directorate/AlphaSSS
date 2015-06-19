0e36262bf88f42a4c51dd6a93663536c128c504e
fractal-overflow <fractal-overflow@alphadirectorate.com>
2015-06-19 21:47:49 +0300

    Added new translation key to registration form

--- content/plugins/alphasss-register/alphasss-register.php
+++ content/plugins/alphasss-register/alphasss-register.php
@@ -423,7 +423,7 @@ add_action( 'plugins_loaded', function(){
 
 				case 22:
 					$field['label']       = str_replace( '%%RegisterEmailLabel%%', __('Your Email Address:', 'alphasss-register'), $field['label'] );
-					$field['description'] = str_replace( '%%RegisterEmailDescription%%', __("We need rest. The spirit is willing, but the flesh is spongy and bruised. Oh sure! Blame the wizards! Ummm…to eBay? Leela, Bender, we're going grave robbing.", 'alphasss-register'), $field['description'] );
+					$field['description'] = str_replace( '%%RegisterEmailDescription%%', __('We need rest. The spirit is willing, but the flesh is spongy and bruised. Oh sure! Blame the wizards! Ummm...to eBay? Leela, Bender, we\'re going grave robbing.', 'alphasss-register'), $field['description'] );
 				break;
 			}
 		}
