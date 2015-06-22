d0e8c11c7bc49776a71ca39aa53d8757aa15d38c
fractal-overflow <fractal-overflow@alphadirectorate.com>
2015-06-22 17:11:25 +0300

    ASSS-447

---
+++ content/plugins/alphasss-gf-finances/alphasss-gf-finances.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: AlphaSSS GF Finances
+ * Plugin URI:  http://alphasss.com/
+ * Description: Alphasss GF Finances
+ * Author:      AlphaSSS
+ * Author URI:  http://alphasss.com
+ * Version:     0.0.1
+ * Text Domain: alphasss-gf-finances
+ */
+
+add_action( 'plugins_loaded', function(){
+
+	
+
+});
+?>
