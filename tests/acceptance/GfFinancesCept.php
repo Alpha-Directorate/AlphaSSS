56c107c5b110c01250d50f6e41ba73530015e697
fractal-overflow <fractal-overflow@alphadirectorate.com>
2015-06-23 20:04:16 +0300

    ASSS-450 ASSS-446

---
+++ tests/acceptance/GfFinancesCept.php
@@ -0,0 +1,38 @@
+<?php 
+$I = new AcceptanceTester($scenario);
+$I->resizeWindow(1024, 1024);
+$I->wantTo('see GF finances');
+
+$I->amOnPage('/');
+$I->see('Login','.button');
+$I->click("//a[@class='button' and text()='Login']");
+$I->seeCurrentUrlEquals('/wp/wp-login.php');
+$I->fillField('log','elen');
+$I->fillField('pwd','funkadelicbro87');
+$I->click('Log In');
+$I->seeCurrentUrlEquals('/browse/elen/');
+$I->moveMouseOver("//li[@id='wp-admin-bar-my-account']");
+$I->see('Finances', 'li');
+$I->moveMouseOver("//a[@class='ab-item' and text()='Finances']");
+$I->click('Accounting');
+$I->wait(3);
+$I->seeCurrentUrlEquals('/browse/elen/finances/');
+$I->moveMouseOver("//li[@id='wp-admin-bar-my-account']");
+$I->see('Log Out','.ab-item');
+$I->click("//a[@class='ab-item' and text()='Log Out']");
+
+// Finances dosn't work for member
+$I->amOnPage('/');
+$I->see('Login','.button');
+$I->click("//a[@class='button' and text()='Login']");
+$I->seeCurrentUrlEquals('/wp/wp-login.php');
+$I->fillField('log','saybb');
+$I->fillField('pwd','funkadelicbro87');
+$I->click('Log In');
+$I->seeCurrentUrlEquals('/browse/saybb/');
+$I->moveMouseOver("//li[@id='wp-admin-bar-my-account']");
+$I->dontSee('Finances', 'li');
+$I->amOnPage('/browse/elen/finances/');
+$I->moveMouseOver("//li[@id='wp-admin-bar-my-account']");
+$I->see('Log Out','.ab-item');
+$I->click("//a[@class='ab-item' and text()='Log Out']");
