5a4829fa15450cd3bd4aaac49f30d02d02b9ac03
fractal-overflow <fractal-overflow@alphadirectorate.com>
2015-06-23 18:25:00 +0300

    ASSS-447 ASSS-448 Migration BugFix

--- migrations/20150623075938_sprint16.php
+++ migrations/20150623075938_sprint16.php
@@ -15,11 +15,11 @@ class Sprint16 extends AbstractMigration
      *
      * More information on writing migrations is available here:
      * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     */
     public function change()
     {
 
     }
+    */
 
     /**
      * Migrate Up.
