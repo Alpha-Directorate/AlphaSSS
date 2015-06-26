1ee853dfdbdfce6cb28318f2751d34bfe6aefd76
fractal-overflow <fractal-overflow@alphadirectorate.com>
2015-06-26 22:31:29 +0300

    ASSS-495 ASSS-496 Test fixed

--- tests/acceptance/GfFinancesCept.php
+++ tests/acceptance/GfFinancesCept.php
@@ -1,9 +1,9 @@
 <?php 
 $I = new AcceptanceTester($scenario);
 $I->resizeWindow(1024, 1024);
-$I->wantTo('see GF finances');
+$I->wantTo('see GF Financials');
 
-// Finances works for the administrator
+// Financials works for the administrator
 $I->amOnPage('/');
 $I->see('Login','.button');
 $I->click("//a[@class='button' and text()='Login']");
@@ -13,8 +13,8 @@ $I->fillField('pwd','#caRousal.72');
 $I->click('Log In');
 $I->seeCurrentUrlEquals('/browse/founder_counselor/');
 $I->moveMouseOver("//li[@id='wp-admin-bar-my-account']");
-$I->see('Finances', 'li');
-$I->moveMouseOver("//a[@class='ab-item' and text()='Finances']");
+$I->see('Financials', 'li');
+$I->moveMouseOver("//a[@class='ab-item' and text()='Financials']");
 $I->click('Accounting');
 $I->seeCurrentUrlEquals('/browse/founder_counselor/accounting/');
 $I->moveMouseOver('#wp-admin-bar-site-name');
@@ -38,8 +38,8 @@ $I->fillField('pwd','funkadelicbro87');
 $I->click('Log In');
 $I->seeCurrentUrlEquals('/browse/elen/');
 $I->moveMouseOver("//li[@id='wp-admin-bar-my-account']");
-$I->see('Finances', 'li');
-$I->moveMouseOver("//a[@class='ab-item' and text()='Finances']");
+$I->see('Financials', 'li');
+$I->moveMouseOver("//a[@class='ab-item' and text()='Financials']");
 $I->click('Accounting');
 $I->seeCurrentUrlEquals('/browse/elen/accounting/');
 $I->see('Congratulations! You are now at the highest level');
@@ -52,7 +52,7 @@ $I->moveMouseOver("//li[@id='wp-admin-bar-my-account']");
 $I->see('Log Out','.ab-item');
 $I->click("//a[@class='ab-item' and text()='Log Out']");
 
-// Finances dosn't work for the member
+// Financials dosn't work for the member
 $I->amOnPage('/');
 $I->see('Login','.button');
 $I->click("//a[@class='button' and text()='Login']");
@@ -62,14 +62,14 @@ $I->fillField('pwd','funkadelicbro87');
 $I->click('Log In');
 $I->seeCurrentUrlEquals('/browse/saybb/');
 $I->moveMouseOver("//li[@id='wp-admin-bar-my-account']");
-$I->dontSee('Finances', 'li');
+$I->dontSee('Financials', 'li');
 $I->amOnPage('/browse/saybb/accounting/');
 $I->see("Not Found");
 $I->moveMouseOver("//li[@id='wp-admin-bar-my-account']");
 $I->see('Log Out','.ab-item');
 $I->click("//a[@class='ab-item' and text()='Log Out']");
 
-// Finances dosn't work for the pre-member
+// Financials dosn't work for the pre-member
 $I->amOnPage('/');
 $I->see('Login','.button');
 $I->click("//a[@class='button' and text()='Login']");
@@ -79,7 +79,7 @@ $I->fillField('pwd','funkadelicbro87');
 $I->click('Log In');
 $I->seeCurrentUrlEquals('/register-pre-member/');
 $I->moveMouseOver("//li[@id='wp-admin-bar-my-account']");
-$I->dontSee('Finances', 'li');
+$I->dontSee('Financials', 'li');
 $I->amOnPage('/browse/tanya/accounting/');
 $I->see("Not Found");
 $I->moveMouseOver("//li[@id='wp-admin-bar-my-account']");
