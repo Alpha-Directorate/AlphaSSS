fd20292af97c90b6f9d2fc0af5e6d187bcec653b
fractal-overflow <fractal-overflow@alphadirectorate.com>
2015-06-29 13:26:50 +0300

    Added Export/Import customizer plugin

---
+++ migrations/20150629102440_sprint17.php
@@ -0,0 +1,43 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+// Load all the admin APIs, for convenience
+require_once 'wordpress-api.php';
+//--
+
+class Sprint17 extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     *
+    public function change()
+    {
+    }
+    */
+
+        /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        if ( $plugin = ( new \WP_CLI\Fetchers\Plugin )->get( 'customizer-export-import' ) ) {
+            activate_plugin( $plugin->file, '' );
+        }
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        if ( $plugin = ( new \WP_CLI\Fetchers\Plugin )->get( 'customizer-export-import' ) ) {
+            deactivate_plugins( $plugin->file, '' );
+        }
+    }
+}
