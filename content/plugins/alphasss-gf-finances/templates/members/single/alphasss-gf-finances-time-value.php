a9ffab2369be3139fb050d36d232d3af9b8eac2e
fractal-overflow <fractal-overflow@alphadirectorate.com>
2015-07-01 13:57:17 +0300

    ASSS-477

--- content/plugins/alphasss-gf-finances/templates/members/single/alphasss-gf-finances-time-value.php
+++ content/plugins/alphasss-gf-finances/templates/members/single/alphasss-gf-finances-time-value.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit;?>
 
-<table class="table" cellspacing="1">             
+<table class="table" id="time-value-table" cellspacing="1">             
 	<thead>
 		<tr> 
 			<th class="text-center"><?php _e('Check to Edit', 'alphasss');?></th> 
@@ -11,10 +11,22 @@
 	<tbody>
 		<?php foreach ([30, 45, 60, 90, 120] as $value):?>
 		<tr> 
-			<td class="text-center"><input type="checkbox" /></td> 
+			<td class="text-center"><input class="time-value-checkbox" type="checkbox" /></td> 
 			<td class="text-center"><?php printf(__('%d min'), $value); ?></td> 
 			<td class="text-center"><a>not used</a></td> 
 		</tr>
 	<?php endforeach;?>
 	</tbody> 
 </table>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.time-value-checkbox').click(function(){
+			if ($(this).attr('checked') == 'checked'){
+				$(this).parent('td').next('td').next('td').html('<input value="0" class="input-small" /> credits');
+			} else {
+				$(this).parent('td').next('td').next('td').html('<a>not used</a>');
+			}
+		});
+	});
+</script>
