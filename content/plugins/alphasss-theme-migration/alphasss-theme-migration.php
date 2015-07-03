147702958e3fa083399f32fd70275b326e0f6b5d
fractal-overflow <fractal-overflow@alphadirectorate.com>
2015-07-03 20:27:44 +0300

    ASSS-499

--- content/plugins/alphasss-theme-migration/alphasss-theme-migration.php
+++ content/plugins/alphasss-theme-migration/alphasss-theme-migration.php
@@ -17,7 +17,7 @@ require_once 'class-option.php';
 add_action( 'plugins_loaded', function(){
 
 	// Check is is_updated is empty or 0
-	if ( ! @file_get_contents(BASEPATH . 'migrations/theme/is_updated' ) ) {
+	if ( ! intval( @file_get_contents(BASEPATH . 'migrations/theme/is_updated' ) ) ) {
 		
 		global $wp_customize;
 
