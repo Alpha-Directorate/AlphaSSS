9416bf4174c2a63c7ed4ec8db9e4bb52cd58daf8
fractal-overflow <fractal-overflow@alphadirectorate.com>
2015-07-03 16:01:07 +0300

    ASSS-245

--- content/plugins/alphasss-theme-migration/alphasss-theme-migration.php
+++ content/plugins/alphasss-theme-migration/alphasss-theme-migration.php
@@ -9,37 +9,71 @@
  * Version:     0.0.1
  * Text Domain: alphasss
  */
- 
- add_action('customize_save_after', function($wp_customize){
- 	
-		$template = get_template();
-		$mods     = get_theme_mods();
-		$data     = [
-			'template'  => $template,
-			'mods'	  => $mods ? $mods : array(),
-			'options'	  => array()
-		];
-		
-		// Get options from the Customizer API.
-		$settings = $wp_customize->settings();
+
+add_action( 'plugins_loaded', function(){
 	
-		foreach ( $settings as $key => $setting ) {
+	if ( file_get_contents(BASEPATH . 'migrations/theme/is_updated' ) ) {
+		
+		global $wp_customize;
+
+		$data = @unserialize( file_get_contents( BASEPATH . 'migrations/theme/theme-customize.dat' ) );
+		
+		// Import custom options.
+		if ( isset( $data['options'] ) ) {
 			
-			if ( 'option' == $setting->type ) {
+			foreach ( $data['options'] as $option_key => $option_value ) {
 				
-				// Don't save widget data.
-				if ( stristr( $key, 'widget_' ) ) {
-					continue;
-				}
+				$option = new WP_Customize_Setting( $wp_customize, $option_key, array(
+					'default'		=> '',
+					'type'			=> 'option',
+					'capability'	=> 'edit_theme_options'
+				) );
 				
-				// Don't save sidebar data.
-				if ( stristr( $key, 'sidebars_' ) ) {
-					continue;
-				}
-				
-				$data['options'][ $key ] = $setting->value();
+				$option->update( $option_value );
 			}
 		}
-		// Serialize the export data.
-		file_put_contents( BASEPATH . 'migrations/theme/theme-customize.dat', serialize( $data ));
- });
+		
+		// Loop through the mods.
+		foreach ( $data['mods'] as $key => $val ) {
+			
+			// Save the mod.
+			set_theme_mod( $key, $val );
+		}
+
+		file_put_contents( BASEPATH . 'migrations/theme/is_updated', 0 );
+	}
+});
+ 
+add_action('customize_save_after', function($wp_customize){
+	
+	$template = get_template();
+	$mods     = get_theme_mods();
+	$data     = [
+		'template'  => $template,
+		'mods'	  => $mods ? $mods : array(),
+		'options'	  => array()
+	];
+	
+	// Get options from the Customizer API.
+	$settings = $wp_customize->settings();
+
+	foreach ( $settings as $key => $setting ) {
+		
+		if ( 'option' == $setting->type ) {
+			
+			// Don't save widget data.
+			if ( stristr( $key, 'widget_' ) ) {
+				continue;
+			}
+			
+			// Don't save sidebar data.
+			if ( stristr( $key, 'sidebars_' ) ) {
+				continue;
+			}
+			
+			$data['options'][ $key ] = $setting->value();
+		}
+	}
+	// Serialize the export data.
+	file_put_contents( BASEPATH . 'migrations/theme/theme-customize.dat', serialize( $data ));
+});
